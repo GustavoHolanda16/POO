ac09ceb8ebb367a48741adce71e9f8f997a4b4cf
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2023-10-31 19:44:25 -0300

    teste2

--- JS_ON.js
+++ JS_ON.js
@@ -13,3 +13,22 @@ const animal= {
     sons(){ console.log('miau miau')}
 }
 console.log(animal)
+console.log(animal.tipo)
+console.log(animal.nome)
+animal.sons()
+
+const livro = {}
+livro.autor = 'Jane Smith'
+console.log(livro)
+livro.anoPublicacao=2006
+console.log(livro)
+delete livro.anoPublicacao
+console.log(livro)
+
+const funcionario = {}
+funcionario['ocupação'] = 'vendas'
+funcionario['viaja frequentemente'] = true
+funcionario['primeiro territorio'] = 'Chicago'
+funcionario['25'] = 'total de clientes'
+
+console.log(funcionario)
