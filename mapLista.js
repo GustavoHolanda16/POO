a711a19d821f1ce3a4a140df71f7bb791908c9cd
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2023-11-06 18:39:52 -0300

    json

--- mapLista.js
+++ mapLista.js
@@ -1,73 +1,18 @@
-
-
-
-
-
 const prompt = require('prompt-sync')()
-const frutasMap ={
-    "0": "maca",
-    "1": "acerola",
-    "2": "laranja",
-    "3": "uva",
-    "4": "kiwi",
+let y = prompt('Digite quantos alunos você irá adicionar: ')
+const turma1={
 }
-let continuar = true
-//loop para teste
-while(continuar){
-  const numeroAleatorio = Math.floor(Math.random() * 5) 
-let x = prompt('Digite uma fruta aqui e veremos se vc acerta: ')
-let y = frutasMap[numeroAleatorio]
-console.log(y)
-const meuArray = y.split('')
-const array2 = x.split('')
-console.log(y)
-console.log(x)
-//verificação de condicionais para igualdade
-if (x == y) {
-  console.log('Acertou a palavra é: ' + x)
+const turma2={
 }
-else {
-  let letras=[]
-  for (let i = 0; i < meuArray.length; i++) {
-    for (let j = 0; j < array2.length; j++) {
-      if (array2[j] == meuArray[i]) {
-        letras[i]=meuArray[i]
-      }
-    }
-  }
-  console.log('Errou! você digitou ' + x + ', e a resposta certa era: ')
-  console.log(letras)
-  
-} 
-//inicio para finaliazação ou repetição do teste
-console.log('quer tentar de novo?')
-let z = prompt('Digite sim para continuar e nao para sair: ')
-
-if(z=='sim'){
-  console.log('você vai tentar de novo!')
-  console.log('espere...')
-  console.clear
-  continuar=true
+for ( let j = 0; j<y; j++){
+  let x = prompt('Digite no nome do aluno: ')
+  turma1[j]=x
 }
-else if (z=='nao') {
-  console.log('você vai desistir!')
-  console.log('espere...')
-  continuar=false
+for ( let j = 0; j<y; j++){
+  let x = prompt('Digite no nome do aluno: ')
+  turma2[j]=x
 }
-else {
-  console.log('digitou errado paizão, ai vc me quebra!')
-  console.log('vaou perguntar novamente! ')
-  let p = prompt('Digite sim para continuar e nao para sair: ')
-  if(p=='sim'){
-    console.log('você vai tentar de novo!')
-    console.log('espere...')
-    console.clear
-    continuar=true
-  }
-  else if (p=='nao') {
-    console.log('você vai desistir!')
-    console.log('espere...')
-    continuar=false
-  }
-}
+for ( let j = 0; j<y; j++){
+  console.log(turma1[j])
+  console.log(turma2[j])
 }
